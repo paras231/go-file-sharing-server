d83e924587a67c330c520adf96d2bc27116a7f69
paras231 <sharmaparas553@gmail.com>
2025-03-22 11:27:04 +0530

    created a handler for streaming video from a static video file

--- main.go
+++ main.go
@@ -37,9 +37,40 @@ func streamLargeFileHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// stream a video
+
+func streamVideo(w http.ResponseWriter, req *http.Request) {
+
+	file, err := os.Open("imgs/pricevideo.mp4")
+	if err != nil {
+		http.Error(w, "Failed to open file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	// Set the content type to video/mp4
+	w.Header().Set("Content-Type", "video/mp4")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = io.Copy(w, file)
+	if err != nil {
+		fmt.Println("Error streaming video:", err)
+		return
+	}
+	flusher.Flush()
+}
+
 func main() {
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/download", streamLargeFileHandler)
+	http.HandleFunc("/stream-video", streamVideo)
+	fmt.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
